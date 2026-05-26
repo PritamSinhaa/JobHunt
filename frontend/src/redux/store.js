9083c8be27069b4e7daae84d8d1d2bb0e00c13f1
Sajjad Ali <rizvisajjad80@gmail.com>
2026-05-27 01:40:46 +0530

    Done with students page

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,47 @@
-import {configureStore} from '@reduxjs/toolkit';
-import authSlice from './authSlice'
-import jobSlice from './jobSlice'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+import jobSlice from "./jobSlice";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
+// import { default as storage } from 'redux-persist/lib/storage'
+
+// store.js - replace the storage import entirely
+const storage = {
+  getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+  setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
+  removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
+}
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const rootReducer = combineReducers({
+    auth:authSlice,
+    job:jobSlice,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 
 const store = configureStore({
-    reducer:{
-        auth: authSlice,
-        job: jobSlice
-    }
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
 export default store;
