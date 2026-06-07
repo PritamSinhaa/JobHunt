f72788c7c3252d114fb006401af347d5dd418e66
Sajjad <rizvisajjad80@gmail.com>
2026-06-07 14:20:29 +0530

    Done with making things dynamc, FilterSearch and companies table done

--- frontend/src/components/admin/Comapanies.jsx
+++ frontend/src/components/admin/Comapanies.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../shared/Navbar";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import CompaniesTable from "./CompaniesTable";
 import { useNavigate } from "react-router-dom";
+import useGetAllCompanies from "@/hooks/useGetAllCompanies";
+import { useDispatch } from "react-redux";
+import { setSearchCompanyByText } from "@/redux/companySlice";
 
 function Companies() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  useGetAllCompanies();
+  const [input, setInput] = useState('');
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(setSearchCompanyByText(input));
+  },[input]);
   return (
+
     <div>
       <Navbar />
       <div className="  max-w-6xl mx-auto my-10">
         <div className="flex items-center justify-between my-5">
-          <Input type="text" className="w-fit" placeholder="Filter by name" />
-          <Button onClick={()=> navigate("/admin/companies/create")}>New Company</Button>
+          <Input
+            onChange={(e)=>setInput(e.target.value)}
+          type="text" className="w-fit" placeholder="Filter by name" />
+          <Button onClick={() => navigate("/admin/companies/create")}>New Company</Button>
         </div>
-        <CompaniesTable/>
+        <CompaniesTable />
       </div>
     </div>
   );
--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -12,8 +12,23 @@ import {
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Edit2, MoreHorizontal } from "lucide-react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function CompaniesTable() {
+  const { companies, searchCompanyByText } = useSelector(store => store.company);
+  const [filterCompany,setFilterCompany] = useState(companies);
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    const filteredCompany = companies.length >= 0 && companies.filter((company)=>{
+      if(!searchCompanyByText){
+        return true
+      };
+      return company?.name?.toLowerCase().includes(searchCompanyByText.toLowerCase())
+    } );
+    setFilterCompany(filteredCompany)
+  },[companies,searchCompanyByText])
   return (
     <div>
       <Table>
@@ -31,32 +46,54 @@ function CompaniesTable() {
         </TableHeader>
 
         <TableBody>
-          <TableRow>
-            <TableCell>
-              <Avatar>
-                <AvatarImage src="https://static.vecteezy.com/system/resources/previews/047/656/219/non_2x/abstract-logo-design-for-any-corporate-brand-business-company-vector.jpg" />
-              </Avatar>
-            </TableCell>
+          {
+            companies.length <= 0 ? (
+            <TableRow>
+              <TableCell colSpan={4} className="text-center">
+                No Company found
+              </TableCell>
+            </TableRow>
+            ) : (
+              <>
+                {
+                  filterCompany?.map((company) => {
+                    return (
+                      <TableRow key={company._id}  >
+                        <TableCell>
+                          <Avatar>
+                            <AvatarImage src={company.logo} />
+                          </Avatar>
+                        </TableCell>
 
-            <TableCell>Company Name</TableCell>
+                        <TableCell>{company.name}</TableCell>
 
-            <TableCell>12-09-2025</TableCell>
+                        <TableCell>{company.createdAt.split('T'[0])}</TableCell>
 
-            <TableCell className="text-right">
-              <Popover>
-                <PopoverTrigger>
-                  <MoreHorizontal />
-                </PopoverTrigger>
+                        <TableCell className="text-right">
+                          <Popover>
+                            <PopoverTrigger>
+                              <MoreHorizontal />
+                            </PopoverTrigger> 
 
-                <PopoverContent className="w-32">
-                  <div className="flex items-center gap-2 w-fit cursor-pointer">
-                    <Edit2 className="w-4" />
-                    <span>Edit</span>
-                  </div>
-                </PopoverContent>
-              </Popover>
-            </TableCell>
-          </TableRow>
+                            <PopoverContent className="w-32">
+                              <div onClick={()=>navigate(`/admin/companies/${company?._id}`)} className="flex items-center gap-2 w-fit cursor-pointer">
+                                <Edit2 className="w-4" />
+                                <span>Edit</span>
+                              </div>
+                            </PopoverContent>
+                          </Popover>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })
+
+                }
+
+              </>
+
+            )
+          }
+
         </TableBody>
       </Table>
     </div>
